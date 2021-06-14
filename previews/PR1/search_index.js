b4de59401b91f996cfd2b5e1d9351438b7bba652
Documenter.jl <documenter@juliadocs.github.io>
2021-06-14 17:04:59 +0000

    build based on 9e210f2

---
+++ previews/PR1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VirtualEcologyLab.jl","page":"Index","title":"VirtualEcologyLab.jl","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"VirtualEcologyLab.jl is a Julia library for simulating ecological and evolutionary experiments. VEL.jl aims to create a  \"virtual laboratory\" (Grimm & Railsback 2005) for ecological and evolutionary processes.","category":"page"}]
+}
